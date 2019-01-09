c07dcc5f610f42a91281c7b3c89c2511297a329f
Ed Cole <edcole@mail.sunyjcc.edu>
2019-01-09 17:48:29 -0500

    Flashcards: add code to process answers

--- flashcards/arithmetic.js
+++ flashcards/arithmetic.js
@@ -31,7 +31,28 @@ var showCard = function (op1, op2, operator, answer) {
     card.find('.operator').text(operator);
     flashcardSlot.contents().remove();
     card.appendTo(flashcardSlot);
+    var answerField = card.find('input[name="answer"]');
+    var form = card.find('form');
+    var getAnswer = $.Deferred();
+    form.submit(function (event) {
+        getAnswer.resolve({op1: op1, op2: op2,
+                           operator: operator, answer: answer,
+                           hisAnswer: answerField.val()});
+        return false;
+    });
+    getAnswer.then(function (cardData) {
+        if (cardData.answer === Number(cardData.hisAnswer)) {
+            console.log(cardData, 'is correct');
+            score(card, true);
+        } else {
+            console.log(cardData, 'is incorrect');
+            score(card, false);
+        }
+        showScore();
+    })/* .always(function () {
+        card.remove();
+       })*/;
 };
 
 
-showCard(12,10, '-', 22);
+showCard(12,10, '-', 2);
