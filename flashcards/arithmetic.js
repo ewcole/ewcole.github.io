8e5e82aeeff205c2364a1ead9f96f0620485b826
Ed Cole <edcole@mail.sunyjcc.edu>
2019-01-10 17:46:16 -0500

    Display correct or incorrect flashcard answer.

--- flashcards/arithmetic.js
+++ flashcards/arithmetic.js
@@ -7,8 +7,11 @@ console.log(flashcardTemplate);
 var answerFormTemplate = $('#answerFormTemplate').text();
 console.log(flashcardTemplate);
 var flashcardSlot = $('#flashcard-slot');
-
+var rightAnswerTemplateText = $('#rightAnswerTemplate').text();
+var wrongAnswerTemplateText = $('#wrongAnswerTemplate').text();
 var scorecard = {total: 0, correct: 0, incorrect: 0};
+// nextCard will be defined below.
+var nextCard;
 
 var score = function(card, isCorrect) {
     scorecard.total++;
@@ -20,8 +23,8 @@ var score = function(card, isCorrect) {
 };
 
 var showScore = function () {
-    $('#stats span.rightAnswers').txt(scorecard.correct);
-    $('#stats span.wrongAnswers').txt(scorecard.incorrect);
+    $('#stats span.rightAnswers').text(scorecard.correct);
+    $('#stats span.wrongAnswers').text(scorecard.incorrect);
 };
 
 var showCard = function (op1, op2, operator, answer) {
@@ -41,18 +44,39 @@ var showCard = function (op1, op2, operator, answer) {
         return false;
     });
     getAnswer.then(function (cardData) {
+        var ga = card.find('.getAnswer');
+        ga.find('*').remove();
+        ga.text(cardData.hisAnswer);
         if (cardData.answer === Number(cardData.hisAnswer)) {
             console.log(cardData, 'is correct');
-            score(card, true);
+            var answerBlock = $(rightAnswerTemplateText);
+            answerBlock.find('.answer').text(cardData.answer);
+            answerBlock.appendTo(card.find('.answer'));
+            score(cardData, true);
         } else {
             console.log(cardData, 'is incorrect');
-            score(card, false);
+            var answerBlock = $(wrongAnswerTemplateText);
+            answerBlock.find('.answer').text(cardData.answer);
+            answerBlock.find('.wrong-answer').text(cardData.hisAnswer);
+            answerBlock.appendTo(card.find('.answer'));
+            score(cardData, false);
         }
         showScore();
+        // Now show the next button
+        var next = $('<form id="next-card-form" action="#"><input type="submit" value="Next"/></form>');
+        card.append(next);
+        next.submit(function () {
+            card.remove();
+            nextCard();
+            return false;
+        });
     })/* .always(function () {
         card.remove();
        })*/;
 };
 
+nextCard = function () {
 
+}
 showCard(12,10, '-', 2);
+
